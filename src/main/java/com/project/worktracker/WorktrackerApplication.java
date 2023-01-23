bb992a14c3f727653fb11e9d7c9ddccdc6ace120
dev-repository01 <123305284+dev-repository01@users.noreply.github.com>
2023-01-23 10:44:25 +0530

    Initial commit

---
+++ src/main/java/com/project/worktracker/WorktrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.worktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/worktracker/WorktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.worktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
